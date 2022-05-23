7804264b8cd6ac6ea0eaacc18273d09eb09682b4
Hano2001 <haraldnorlin93@gmail.com>
2022-05-23 23:03:45 +0200

    counter in place

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [pizzaCounter, setPizzaCounter] = useState(0);
+  
+  useEffect(() => {
+
+  },[pizzaCounter])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>PIZZA TIME</h1>
+     <h2>{pizzaCounter}</h2>
+     <button onClick={() => {setPizzaCounter(pizzaCounter +1)}}>Pizza</button>
     </div>
   );
 }
