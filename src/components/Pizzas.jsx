985b84e6bdaa0ef35bdc694eaf120068f582212e
Hano2001 <haraldnorlin93@gmail.com>
2022-08-23 17:00:57 +0200

    upgrades calc amount finally in place

--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -4,19 +4,30 @@ import { CounterContext } from "../contexts/CounterContext";
 export default function Pizzas({ count }) {
   const { pizzaCounter, setPizzaCounter, upgradesData, setupgradesData } =
     useContext(CounterContext);
+  const [upgradesValue, setUpgradesValue] = useState(0);
 
+  useEffect(() => {
+    let newAmount = 0;
+    upgradesData.map((pizza, key) => {
+      newAmount += pizza.amount * pizza.generate;
+    });
+    setUpgradesValue(newAmount);
+  }, [upgradesData]);
   const TenSeconds_MS = 1000;
   useEffect(() => {
+    let newAmount = 0;
+    upgradesData.map((pizza, key) => {
+      newAmount += pizza.amount * pizza.generate;
+    });
+    setUpgradesValue(newAmount);
     const interval = setInterval(() => {
-      // let amount = 0;
-      // upgradesData.map((pizza, key) => {
-      //   if (pizza.amount > 0) {
-      //     amount += pizza.amount * pizza.generate;
-          
-          
-      //   }
-      // });
-      //setPizzaCounter(pizzaCounter + amount);
+      let amount = 0;
+      upgradesData.map((pizza, key) => {
+        if (pizza.amount > 0) {
+          amount += pizza.amount * pizza.generate;
+        }
+      });
+      setPizzaCounter(pizzaCounter => pizzaCounter+ amount);
     }, TenSeconds_MS);
 
     return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
@@ -25,13 +36,12 @@ export default function Pizzas({ count }) {
   function ButtonClick(generate, price, index) {
     setPizzaCounter((pizzaCounter) => pizzaCounter - price);
     let pizzaArray = [...upgradesData];
-    pizzaArray[index].amount += 1; 
+    pizzaArray[index].amount += 1;
 
     setupgradesData(pizzaArray);
   }
   return (
     <div>
-      <button onClick={() => console.log(upgradesData)}>TEST</button>
       {upgradesData.map((pizza, index) => {
         return (
           <div>
@@ -55,7 +65,6 @@ export default function Pizzas({ count }) {
           );
         })}
       </ul>
-      <button onClick={() => console.log(pizzaCounter)}>Check counter</button>
     </div>
   );
 }
