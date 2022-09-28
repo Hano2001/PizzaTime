8431f9cf8293df08c6f27fe92883e6f6ecc6162e
Hano2001 <haraldnorlin93@gmail.com>
2022-09-28 22:46:22 +0200

    general styling fixes

--- src/App.js
+++ src/App.js
@@ -2,40 +2,47 @@ import React, { useEffect, useState } from "react";
 import Pizzas from "./components/Pizzas";
 import Upgrades from "./components/Upgrades";
 import { CounterContext } from "./contexts/CounterContext";
-import { generationData} from "./data/generationData";
-import { Button, Footer, MainDiv } from "./styling/generalStyling";
+import { generationData } from "./data/generationData";
+import { Button, Counter, Footer, MainDiv,Container } from "./styling/generalStyling";
+
 function App() {
   const [pizzaCounter, setPizzaCounter] = useState(0);
   const [generateData, setGenerateData] = useState(generationData);
   const contextValues = {
     pizzaCounter,
-    setPizzaCounter, 
+    setPizzaCounter,
     generateData,
     setGenerateData,
   };
-
 
   useEffect(() => {}, [pizzaCounter]);
   return (
     <MainDiv>
       <CounterContext.Provider value={contextValues}>
         <h1>PIZZA TIME</h1>
-        
-        <h2>{pizzaCounter}</h2>
-        <Button
-          function="clicker"
-          onClick={() => {
-            setPizzaCounter(pizzaCounter + 1);
-          }}
-        >
-          Pizza
-        </Button>
+        <Container>
+        <Upgrades />
+        <Counter>
+          <h2>{pizzaCounter}</h2>
+          <Button
+            function="clicker"
+            onClick={() => {
+              setPizzaCounter(pizzaCounter + 1);
+            }}
+          >
+            Pizza
+          </Button>
+        </Counter>
         <Pizzas count={pizzaCounter} />
-        <Upgrades/>
+
+        </Container>
       </CounterContext.Provider>
       <Footer>
-      <a href="https://github.com/Hano2001" target="_blank">The creators GitHub</a>
+        <a href="https://github.com/Hano2001" target="_blank">
+          The creators GitHub
+        </a>
       </Footer>
+      
     </MainDiv>
   );
 }
